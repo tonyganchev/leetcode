1e25fd93686a5512f839a238a3a315855d6767fb
Tony Ganchev <tony.ganchev@gmail.com>
2025-12-07 16:14:16 +0000

    aoc24-11

---
+++ advent-of-code/2024/aoc24-11.py
@@ -0,0 +1,33 @@
+from math import ceil, log10
+
+def blink(stone, times):
+    if times > 213:
+        print(f'  d {stone}: {times}')
+    if times == 0:
+        return 1
+    times -= 1
+    if stone == 0:
+        return blink(1, times)
+    else:
+        m = ceil(log10(stone))
+        if m > 0 and m % 2 == 0:
+            p = 10 ** (m // 2)
+            return blink(stone // p, times) + blink(stone % p, times)
+        else:
+            return blink(stone * 2024, times)
+
+def solve(input, times):
+    stones = [int(s) for s in input.split(' ')]
+    result = 0
+    for s in stones:
+        r = blink(s, times)
+        print(f'{s}: {r}')
+        result += r
+    return result
+
+def part1(input):
+    return solve(input, 25)
+def part2(input):
+    return solve(input, 75)
+# print(solve('125 17',25))
+print(solve('5 127 680267 39260 0 26 3553 5851995', 2))
