49469a876a00e63b9be5745945f8932435a2a979
Tony Ganchev <tony.ganchev@gmail.com>
2025-12-20 14:51:01 +0200

    lc 1866

--- 1866.number-of-ways-to-rearrange-sticks-with-k-sticks-visible.py
+++ 1866.number-of-ways-to-rearrange-sticks-with-k-sticks-visible.py
@@ -9,42 +9,18 @@ from typing import List
 # @lc code=start
 
 from functools import cache
-from itertools import combinations
-
-@cache
-def fact(n):
-    assert n >= 0
-    return 1 if n <= 1 else n * fact(n - 1)
-
-def count_sequences_optimized(n, barriers):
-    # Rule: If barriers are empty or k=1, result is (n-1)!
-    k = len(barriers)
-    if k <= 1:
-        return fact(n - 1)
-    
-    # The Magic Formula
-    numerator = fact(n - 1)
-    
-    denominator = 1
-    last_barrier = barriers[-1]
-    
-    # Product of (Last_Barrier - Current_Barrier) for all previous barriers
-    for i in range(k - 1):
-        denominator *= (last_barrier - barriers[i])
-        
-    return numerator // denominator
 
 class Solution:
     def rearrangeSticks(self, n: int, k: int) -> int:
-        total_valid_count = 0
-        elements = range(1, n)
-        
-        # Iterate through all combinations of k barriers
-        for b_set in combinations(elements, k - 1):
-            # b_set comes out sorted from combinations, which is what we want
-            total_valid_count += count_sequences_optimized(n, list(b_set) + [n])
-            
-        return total_valid_count % (10 ** 9 + 7)
+        @cache
+        def dp(n, k):
+            if k == 0:
+                return 0
+            if n == k:
+                return 1
+            return (dp(n - 1, k - 1) + (n - 1) * dp(n - 1, k)) % (10 ** 9 + 7)
+
+        return dp(n, k)
 
 # @lc code=end
 
@@ -66,4 +42,4 @@ run_test(method, [14, 11], 91091) # verified
 run_test(method, [15, 11], 2749747) # verified
 run_test(method, [16, 11], 78558480) # verified
 run_test(method, [20, 11], 647427950) # verified
-run_test(method, [105, 20], 647427950) # verified
+run_test(method, [105, 20], 680986848) # verified
